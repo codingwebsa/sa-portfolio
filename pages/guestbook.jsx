c40dcad134b3e47f89123eedc1f0ed6784790795
codingwebsa <codingwebsa@gmail.com>
2023-02-25 18:56:40 +0600

    supabase

--- pages/guestbook.jsx
+++ pages/guestbook.jsx
@@ -1,8 +1,9 @@
 import { Form } from "@/components";
 import { useSession } from "next-auth/react";
 import { SignIn, SignOut } from "@/lib/actions";
+import { supabase } from "@/lib/supabase";
 
-export default function GuestbookPage() {
+export default function GuestbookPage({ comments }) {
   const { data: session } = useSession();
   return (
     <section>
@@ -15,25 +16,28 @@ export default function GuestbookPage() {
       ) : (
         <SignIn />
       )}
-      {/* {entries.map((entry) => (
-        <div key={entry.id} className="flex flex-col space-y-1 mb-4">
-          <div className="w-full text-sm">
-            <span className="text-neutral-600 dark:text-neutral-400 mr-1">
-              {entry.created_by}:
-            </span>
-            {entry.body}
+      <div className="my-8">
+        {comments.map((entry) => (
+          <div key={entry.id} className="flex flex-col space-y-1 mb-4">
+            <div className="w-full text-sm">
+              <span className="text-neutral-600 dark:text-neutral-400 mr-1">
+                {entry.name}:
+              </span>
+              {entry.comment}
+            </div>
           </div>
-        </div>
-      ))} */}
+        ))}
+      </div>
     </section>
   );
 }
 
-// export async function getServerSideProps(ctx) {
-//   const session = await getSession(ctx);
-//   return {
-//     props: {
-//       session,
-//     },
-//   };
-// }
+export async function getServerSideProps(ctx) {
+  let { data } = await supabase.from("comments").select();
+
+  return {
+    props: {
+      comments: data,
+    },
+  };
+}
--- pages/index.jsx
+++ pages/index.jsx
@@ -55,22 +55,21 @@ const Home = () => {
         </p>
         <ul className="flex flex-col md:flex-row mt-8 space-x-0 md:space-x-4 space-y-2 md:space-y-0 font-sm text-neutral-500 dark:text-neutral-400">
           <li>
-            <a
+            <Link
               className="flex items-center hover:text-neutral-700 dark:hover:text-neutral-200 transition-all gap-2"
               rel="noopener noreferrer"
-              target="_blank"
-              href="https://twitter.com/leeerob"
+              href="/"
             >
               <ArrowIcon />
               <p className="h-7">follow me on twitter</p>
-            </a>
+            </Link>
           </li>
           <li>
             <a
               className="flex items-center hover:text-neutral-700 dark:hover:text-neutral-200 transition-all gap-2"
               rel="noopener noreferrer"
               target="_blank"
-              href="https://leerob.substack.com"
+              href="https://codingwebsa.substack.com"
             >
               <ArrowIcon />
               <p className="h-7">get email updates</p>
